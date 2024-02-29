a4fa5f49bc46707a21aba6015ec43a899f4f2642
nicabuhat <cabuhat.nicaela@gmail.com>
2024-03-01 04:42:53 +0800

    FEAT: add animations

--- app/js/_global.js
+++ app/js/_global.js
@@ -1,11 +1,13 @@
 var app = {};
+var counter = 0;
 // Global
-app.global = {
+(app.global = {
   init: function () {
     // Load all global functions here
     console.log("load global functions");
     app.global.logAnchorClicks();
     app.global.modalAction();
+    app.global.animateOnLoad();
   },
   logAnchorClicks: function () {
     const anchors = document.querySelectorAll("A");
@@ -40,7 +42,29 @@ app.global = {
       currentImage.style.display = "none";
     });
   },
-};
+  animateOnLoad: function () {
+    const section = document.querySelectorAll("section")[1];
 
-// Run the global stuff
-app.global.init();
+    document.addEventListener(
+      "scroll",
+      (e) => {
+        const vwTop = window.scrollY;
+        const vwBottom = window.scrollY + window.innerHeight;
+        const elemTop = section.offsetTop;
+        const elemHeight = section.offsetHeight;
+
+        if (counter < 1) {
+          if (vwBottom > elemTop && vwTop - elemHeight < elemTop) {
+            section.classList.add("animate");
+            counter++;
+          } else {
+            section.classList.remove("animate");
+          }
+        }
+      },
+      false
+    );
+  },
+}),
+  // Run the global stuff
+  app.global.init();
